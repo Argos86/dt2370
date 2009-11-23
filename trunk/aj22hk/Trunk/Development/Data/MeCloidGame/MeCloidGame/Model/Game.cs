222de3a4040a1e4ceff6c6926c9b34356baee185
sajberNinja <sajberNinja@3422cff2-cdd9-11de-91bf-0503b81643b9>
2009-11-23 21:07:55 +0000

    Fixed some collision detection issues but far from all of them. aj22hk

--- trunk/aj22hk/Trunk/Development/Data/MeCloidGame/MeCloidGame/Model/Game.cs
+++ trunk/aj22hk/Trunk/Development/Data/MeCloidGame/MeCloidGame/Model/Game.cs
@@ -41,11 +41,75 @@ namespace MeCloidGame.Model
 
         public bool Update(float a_elapsedTime)
         {
-            m_player.UpdateVelocity();
-            m_player.UpdatePosition();
-            HandleCollisions();
+            UpdatePlayer(a_elapsedTime);
 
             return true;
+        }
+
+        public void UpdatePlayer(float a_elapsedTime)
+        {
+            /*Vector2 gravity = new Vector2(0, 9.82f);
+
+            m_player.m_velocity += gravity; //* a_elapsedTime;
+            Vector2 newPos = m_player.m_pos + m_player.m_velocity * a_elapsedTime;
+            Console.WriteLine(m_level.GetCollision((int)m_player.m_pos.X, (int)m_player.m_pos.Y));
+            m_player.m_isOnGround = false;
+            m_player.m_pos = Collide(m_player.m_pos, newPos, new Vector2(1, 1), ref m_player.m_velocity, ref m_player.m_isOnGround);*/
+
+            m_player.UpdateVelocity(a_elapsedTime);
+            Vector2 newPos = m_player.CalculateNewPosition(a_elapsedTime);
+
+            m_player.m_pos = Collide(m_player.m_pos, newPos, new Vector2(1.0f), ref m_player.m_velocity, ref m_player.m_isOnGround);
+            //HandleCollisions();
+        }
+
+        public Vector2 Collide(Vector2 a_oldPos, Vector2 a_newPos, Vector2 a_size, ref Vector2 a_outVelocity, ref bool a_outIsOnGround)
+        {
+            int left = (int)Math.Round(a_newPos.X - m_player.WIDTH / 2);
+            int top = (int)Math.Round(a_newPos.Y - m_player.HEIGHT);
+            Rectangle newPosBounds = new Rectangle(left, top, m_player.WIDTH, m_player.HEIGHT);
+
+            int leftTile = (int)Math.Floor((float)newPosBounds.Left / Tile.WIDTH);
+            int rightTile = (int)Math.Ceiling(((float)newPosBounds.Right / Tile.WIDTH)) - 1;
+            int topTile = (int)Math.Floor((float)newPosBounds.Top / Tile.HEIGHT);
+            int bottomTile = (int)Math.Ceiling(((float)newPosBounds.Bottom / Tile.HEIGHT)) - 1;
+
+            m_player.m_isOnGround = false;
+
+            for (int y = topTile; y <= bottomTile; ++y)
+            {
+                for (int x = leftTile; x <= rightTile; ++x)
+                {
+                    Tile.TileType collision = m_level.GetCollision(x, y);
+                    if (collision == Tile.TileType.Solid)
+                    {
+                        // Try x movement
+                        Vector2 xMove = new Vector2(a_newPos.X, a_oldPos.Y);
+                        if (m_level.GetCollision((int)xMove.X, (int)xMove.Y) == Tile.TileType.Solid)
+                        {
+                            a_outVelocity.Y = 0.0f;
+                            a_outIsOnGround = true;
+
+                            //return xMove;
+                            return a_oldPos;
+                        }
+
+                        // Try y movement
+                        Vector2 yMove = new Vector2(a_oldPos.X, a_newPos.Y);
+                        if (m_level.GetCollision((int)yMove.X, (int)yMove.Y) == Tile.TileType.Solid)
+                        {
+                            a_outVelocity.X = 0.0f;
+
+                            //return yMove;
+                            return a_oldPos;
+                        }
+
+                        return a_oldPos;
+                    }
+                }
+            }
+            
+            return a_newPos;
         }
 
         public void HandleCollisions()
