abc056aca746499058b1113918ff8a30a32ac0e8
sajberNinja <sajberNinja@3422cff2-cdd9-11de-91bf-0503b81643b9>
2009-11-24 01:01:24 +0000

    Added some simple debug-tools. aj22hk

--- trunk/aj22hk/Trunk/Development/Data/MeCloidGame/MeCloidGame/Controllers/PlayGame.cs
+++ trunk/aj22hk/Trunk/Development/Data/MeCloidGame/MeCloidGame/Controllers/PlayGame.cs
@@ -47,9 +47,70 @@ namespace MeCloidGame.Controllers
 
             a_model.MovePlayer(movement, isJumping);
 
+            if (Helpers.Settings.Debug)
+            {
+                DrawGrid(a_game, a_elapsedTime);
+            }
+
             m_gameView.Draw(a_game, 1.0f);
 
             return true;
+        }
+
+        private void DrawGrid(Model.Game a_game, float a_elapsedTime)
+        {
+            Helpers.PointBatch gridCenters = new Helpers.PointBatch(m_coreView.m_device, 0.8f, 3);
+            gridCenters.Begin();
+
+            for (int x = 0; x < 20; ++x)
+            {
+                for (int y = 0; y < 15; ++y)
+                {
+                    gridCenters.Batch(new Vector2(x * Model.Tile.WIDTH + Model.Tile.WIDTH / 2, y * Model.Tile.HEIGHT + Model.Tile.HEIGHT / 2), Color.Green);
+                }
+            }
+
+            gridCenters.End();
+
+            Helpers.LineBatch line = new Helpers.LineBatch(m_coreView.m_device, 0.4f);
+            line.Begin();
+
+            for (int x = 0; x < 1280; x += 64)
+            {
+                line.Batch(new Vector2(x, 0), new Vector2(x, 720), Color.Red, 1.0f);
+            }
+
+            for (int y = 0; y < 720; y += 48)
+            {
+                line.Batch(new Vector2(0, y), new Vector2(1280, y), Color.Red, 1.0f);
+            }
+
+            line.End();
+
+            Rectangle bounds = a_game.m_player.BoundingRectangle;
+            Helpers.PointBatch point = new Helpers.PointBatch(m_coreView.m_device, 1.0f, 5);
+            point.Begin();
+
+            point.Batch(a_game.m_player.m_pos, Color.Yellow);
+            point.Batch(new Vector2(a_game.m_player.m_pos.X, a_game.m_player.m_pos.Y - a_game.m_player.HEIGHT), Color.Yellow);
+            point.Batch(new Vector2(a_game.m_player.m_pos.X + a_game.m_player.WIDTH / 2, a_game.m_player.m_pos.Y - a_game.m_player.HEIGHT / 2), Color.Yellow);
+            point.Batch(new Vector2(a_game.m_player.m_pos.X - a_game.m_player.WIDTH / 2, a_game.m_player.m_pos.Y - a_game.m_player.HEIGHT / 2), Color.Yellow);
+
+            point.Batch(new Vector2(bounds.X, bounds.Y), Color.Violet);
+            point.Batch(new Vector2(bounds.X + bounds.Width, bounds.Y), Color.Violet);
+            point.Batch(new Vector2(bounds.X, bounds.Y + bounds.Height), Color.Violet);
+            point.Batch(new Vector2(bounds.X + bounds.Width, bounds.Y + bounds.Height), Color.Violet);
+
+            point.Batch(new Vector2(bounds.Center.X, bounds.Center.Y), Color.RoyalBlue);
+
+            point.End();
+
+            Helpers.LineBatch dirVec = new Helpers.LineBatch(m_coreView.m_device, 1.0f);
+            dirVec.Begin();
+            Vector2 newPos = a_game.m_player.m_pos + a_game.m_player.m_velocity * a_elapsedTime;
+            newPos = new Vector2(newPos.X, newPos.Y - a_game.m_player.HEIGHT / 2);
+            dirVec.Batch(new Vector2(a_game.m_player.m_pos.X, a_game.m_player.m_pos.Y - a_game.m_player.HEIGHT / 2), Color.Red, newPos, Color.Blue, 1.0f);
+            dirVec.End();
         }
 
         #endregion
---
+++ trunk/aj22hk/Trunk/Development/Data/MeCloidGame/MeCloidGame/Helpers/PointBatch.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MeCloidGame.Helpers
+{
+    public sealed class PointBatch
+    {
+        private GraphicsDevice m_graphicsDevice;
+        private List<VertexPositionColor> m_points = new List<VertexPositionColor>();
+        private VertexDeclaration m_vertexDeclaration;
+        private BasicEffect m_basicEffect;
+        private int m_pointSize;
+
+        public PointBatch(GraphicsDevice a_graphicsDevice, float a_alpha, int a_pointSize)
+        {
+            m_graphicsDevice = a_graphicsDevice;
+            m_basicEffect = new BasicEffect(m_graphicsDevice, null);
+            m_basicEffect.VertexColorEnabled = true;
+            m_basicEffect.Alpha = a_alpha;
+            m_basicEffect.Projection = Matrix.CreateOrthographicOffCenter(0.0f, m_graphicsDevice.Viewport.Width, m_graphicsDevice.Viewport.Height, 0.0f, 0.0f, -1.0f);
+            m_basicEffect.View = Matrix.Identity;
+            m_basicEffect.World = Matrix.Identity;
+
+            m_vertexDeclaration = new VertexDeclaration(m_graphicsDevice, VertexPositionColor.VertexElements);
+
+            m_pointSize = a_pointSize;
+        }
+
+        public void Begin()
+        {
+            m_points.Clear();
+        }
+
+        public void Batch(Vector2 a_point, Color a_color)
+        {
+            VertexPositionColor batchPoint = new VertexPositionColor(new Vector3(a_point, 0.0f), a_color);
+
+            m_points.Add(batchPoint);
+        }
+
+        public void End()
+        {
+            if (m_points.Count > 0)
+            {
+                m_graphicsDevice.VertexDeclaration = m_vertexDeclaration;
+                m_graphicsDevice.RenderState.PointSize = m_pointSize;
+                m_graphicsDevice.RenderState.FillMode = FillMode.Solid;
+
+                m_basicEffect.Begin();
+
+                foreach (EffectPass pass in m_basicEffect.CurrentTechnique.Passes)
+                {
+                    pass.Begin();
+
+                    m_graphicsDevice.DrawUserPrimitives<VertexPositionColor>(PrimitiveType.PointList, m_points.ToArray(), 0, m_points.Count);
+
+                    pass.End();
+                }
+
+                m_basicEffect.End();
+            }
+        }
+    }
+}
