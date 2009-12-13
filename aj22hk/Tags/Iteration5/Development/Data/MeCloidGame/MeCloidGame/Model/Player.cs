ceff0e8043367d68f11731b9456f2913d3bfb14c
sajberNinja <sajberNinja@3422cff2-cdd9-11de-91bf-0503b81643b9>
2009-12-13 21:49:20 +0000

    Tag for iteration 5. aj22hk

---
+++ aj22hk/Tags/Iteration5/Development/Data/MeCloidGame/MeCloidGame/Model/Player.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+
+namespace MeCloidGame.Model
+{
+    class Player
+    {
+        // TODO: Redo jumping to more mimic platform starter kit with jumphight depending on time button is pressed.
+        // TODO: Add weapons.
+        // TODO: Make it possible to die.
+        public Vector2 m_pos;
+        public Vector2 m_velocity;
+        public float m_movement;
+
+        public bool m_isOnGround;
+
+        public const float WIDTH = 70.0f / 48.0f;
+        public const float HEIGHT = 130.0f / 48.0f;
+
+        private const float Gravity = 9.82f;
+        private const float MaxFallSpeed = 15.0f;
+        private const float MaxMoveSpeed = 8.0f;
+        private const float Acceleration = 20.0f;
+
+        public bool m_isJumping;
+        private bool m_wasJumping;
+        private float m_jumpTime;
+        private const float MaxJumpTime = 1.0f;
+
+        public Player()
+        {
+            m_pos = new Vector2(2.0f, 47.0f);
+        }
+
+        public bool UpdateVelocity(float a_elapsedTime)
+        {
+            m_velocity.X += m_movement * Acceleration * a_elapsedTime;
+
+            if (m_movement == 0 && m_isOnGround)
+            {
+                m_velocity.X -= m_velocity.X * (float)Math.Pow(2.0f, 2.0f) * a_elapsedTime;
+                if ( Math.Abs(m_velocity.X) < 1.0f)
+                {
+                    m_velocity.X = 0;
+                }
+            }
+
+            if (m_movement == 0 && !m_isOnGround)
+            {
+                m_velocity.X -= m_velocity.X * 1.0f * a_elapsedTime;
+            }
+
+            m_velocity.X = MathHelper.Clamp(m_velocity.X, -MaxMoveSpeed, MaxMoveSpeed);
+            
+            m_velocity.Y += Gravity * a_elapsedTime;
+            m_velocity.Y = MathHelper.Clamp(m_velocity.Y, -MaxFallSpeed, MaxFallSpeed);
+
+            if (m_isJumping && m_isOnGround)
+            {
+                m_velocity.Y = -9.0f;
+            }
+            //m_velocity.Y = DoJump(m_velocity.Y);
+
+            if (!m_isOnGround)
+            {
+                m_velocity.X -= m_velocity.X * 0.5f * a_elapsedTime;
+            }
+
+            if (m_isOnGround)
+            {
+                m_velocity.X -= m_velocity.X * 0.5f * a_elapsedTime;
+            }
+
+            return true;
+        }
+
+        public Vector2 CalculateNewPosition(float a_elapsedTime)
+        {
+            Vector2 newPos = m_pos + m_velocity * a_elapsedTime;
+
+            return newPos;
+        }
+
+        private float DoJump(float a_velY)
+        {
+            if (m_isJumping)
+            {
+                if ((!m_wasJumping && m_isOnGround) || m_jumpTime > 0.0f)
+                {
+                    m_jumpTime += 0.1f;
+                }
+
+                if (m_jumpTime > 0.0f && m_jumpTime <= MaxJumpTime)
+                {
+                    a_velY = -25 * (1.0f - (float)Math.Pow(m_jumpTime / MaxJumpTime, 0.114f));
+                }
+                else
+                {
+                    m_jumpTime = 0.0f;
+                }
+            }
+            else
+            {
+                m_jumpTime = 0.0f;
+            }
+
+            m_wasJumping = m_isJumping;
+
+            return a_velY;
+        }
+    }
+}
---
+++ aj22hk/Tags/Iteration5/Development/Data/MeCloidGame/MeCloidGame/Views/LevelView.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MeCloidGame.Views
+{
+    class LevelView
+    {
+        public void DrawLevel(Model.Level a_level, Core a_coreView, Camera a_camera)
+        {
+            if (Helpers.Settings.Edit)
+            {
+                DrawGrid(a_level, a_coreView, a_camera);
+                DrawTiles(a_level, a_coreView, a_camera);
+            }
+            else
+            {
+                for (int x = 0; x < Model.Level.WIDTH; ++x)
+                {
+                    for (int y = 0; y < Model.Level.HEIGHT; ++y)
+                    {
+                        Vector2 pos = a_camera.Translate(new Vector2(x, y));
+                        Rectangle dest = new Rectangle((int)pos.X, (int)pos.Y, a_camera.m_zoom, a_camera.m_zoom);
+                        Rectangle src;
+                        switch (a_level.Tiles[x, y].Type)
+                        {
+                            case Model.Tile.TileType.Solid:
+                                src = new Rectangle(64 * 0, 48 * 0, 64, 48);
+                                a_coreView.Draw(a_coreView.Textures.Tiles, dest, src, Color.White);
+                                break;
+                            case Model.Tile.TileType.Destroyable:
+                                src = new Rectangle(64 * 1, 48 * 0, 64, 48);
+                                a_coreView.Draw(a_coreView.Textures.Tiles, dest, src, Color.White);
+                                break;
+                            case Model.Tile.TileType.Portal:
+                            case Model.Tile.TileType.PortalFill:
+                                src = new Rectangle(64 * 2, 48 * 0, 64, 48);
+                                a_coreView.Draw(a_coreView.Textures.Tiles, dest, src, Color.White);
+                                break;
+                        }
+                    }
+                }
+
+                if (Helpers.Settings.Debug)
+                {
+                    DrawGrid(a_level, a_coreView, a_camera);
+                }
+            }
+        }
+
+        private void DrawGrid(Model.Level a_level, Core a_coreView, Camera a_camera)
+        {
+            // Grid lines
+            Helpers.LineBatch gridLines = new Helpers.LineBatch(a_coreView.m_device, 0.4f);
+            gridLines.Begin();
+
+            // Vertical lines
+            for (int x = 0; x <= Model.Level.WIDTH; ++x)
+            {
+                gridLines.Batch(new Vector2(x * a_camera.m_zoom - a_camera.TopLeft.X , 0 - a_camera.TopLeft.Y), new Vector2(x * a_camera.m_zoom - a_camera.TopLeft.X, Model.Level.HEIGHT * a_camera.m_zoom - a_camera.TopLeft.Y), Color.Red, 1.0f);
+            }
+
+            // Horizontal lines
+            for (int y = 0; y <= Model.Level.HEIGHT; ++y)
+            {
+                gridLines.Batch(new Vector2(0 - a_camera.TopLeft.X, y * a_camera.m_zoom - a_camera.TopLeft.Y), new Vector2(Model.Level.WIDTH * a_camera.m_zoom - a_camera.TopLeft.X, y * a_camera.m_zoom - a_camera.TopLeft.Y), Color.Red, 1.0f);
+            }
+
+            gridLines.End();
+        }
+
+        private void DrawTiles(Model.Level a_level, Core a_coreView, Camera a_camera)
+        {
+            Helpers.PointBatch tiles = new Helpers.PointBatch(a_coreView.m_device, 0.8f, a_camera.m_zoom);
+            tiles.Begin();
+
+            for (int x = 0; x < Model.Level.WIDTH; ++x)
+            {
+                for (int y = 0; y < Model.Level.HEIGHT; ++y)
+                {
+                    Vector2 pos = a_camera.Translate(new Vector2(x, y));
+                    pos = new Vector2(pos.X + a_camera.m_zoom / 2, pos.Y + a_camera.m_zoom / 2);
+                    switch (a_level.Tiles[x, y].Type)
+                    {
+                        case Model.Tile.TileType.Solid:
+                            tiles.Batch(pos, Color.BlanchedAlmond);
+                            break;
+                        case Model.Tile.TileType.Destroyable:
+                            tiles.Batch(pos, Color.BlueViolet);
+                            break;
+                        case Model.Tile.TileType.Portal:
+                            tiles.Batch(pos, Color.YellowGreen);
+                            break;
+                        case Model.Tile.TileType.PortalFill:
+                            tiles.Batch(pos, Color.Yellow);
+                            break;
+                    }
+                }
+            }
+
+            tiles.End();
+        }
+
+        #region Test
+
+        public void Test(Core a_coreView)
+        {
+            Model.Level testLevel = new Model.Level("test.txt");
+
+            DrawLevel(testLevel, a_coreView, null);
+        }
+
+        #endregion
+    }
+}
